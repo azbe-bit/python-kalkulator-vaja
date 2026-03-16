e8af3def115fb8b681c78379af5b4fad237717bb
azbe-bit <azbe.kriznar@gmail.com>
2026-03-16 20:17:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,45 @@ def sestej(a, b):
 def odstej(a, b):
     return a - b
 
+def pomnozi(a, b):
+    return a * b
+
+def deli(a, b):
+    if b == 0:
+        return "Napaka: deljenje z 0 ni dovoljeno!"
+    return a / b
+
 print("Pozdrav! Mini kalkulator")
 print("1 = seštevanje")
 print("2 = odštevanje")
+print("3 = množenje")
+print("4 = deljenje")
+print("0 = izhod")
 
-izbira = input("Kaj želiš narediti? (1 ali 2): ")
+izbira = input("Kaj želiš narediti? (0,1,2,3 ali 4): ")
 
 if izbira == "1":
     x = float(input("Prvo število: "))
     y = float(input("Drugo število: "))
     print(f"Rezultat: {sestej(x, y)}")
+
 elif izbira == "2":
     x = float(input("Prvo število: "))
     y = float(input("Drugo število: "))
     print(f"Rezultat: {odstej(x, y)}")
+
+elif izbira == "3":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    print(f"Rezultat: {pomnozi(x, y)}")
+
+elif izbira == "4":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    print(f"Rezultat: {deli(x, y)}")
+
+elif izbira == "0":
+    print("Program se zapira. Nasvidenje!")
+
 else:
     print("Neveljavna izbira!")
